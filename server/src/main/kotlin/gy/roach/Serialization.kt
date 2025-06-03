1e01464906bec649822f0e66dbc66a02a3fe8c98
Steve Roach <sroachst@gmail.com>
2025-06-02 20:19:07 -0400

    Add initial setup for templating, routing, and serialization

---
+++ server/src/main/kotlin/gy/roach/Serialization.kt
@@ -0,0 +1,48 @@
+package gy.roach
+
+import com.asyncapi.kotlinasyncapi.context.service.AsyncApiExtension
+import com.asyncapi.kotlinasyncapi.ktor.AsyncApiPlugin
+import com.codahale.metrics.*
+import com.sksamuel.cohort.*
+import com.sksamuel.cohort.cpu.*
+import com.sksamuel.cohort.memory.*
+import dev.hayden.KHealth
+import freemarker.cache.*
+import io.ktor.http.*
+import io.ktor.http.content.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.freemarker.*
+import io.ktor.server.html.*
+import io.ktor.server.http.content.*
+import io.ktor.server.metrics.dropwizard.*
+import io.ktor.server.plugins.cachingheaders.*
+import io.ktor.server.plugins.calllogging.*
+import io.ktor.server.plugins.compression.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.defaultheaders.*
+import io.ktor.server.plugins.openapi.*
+import io.ktor.server.plugins.partialcontent.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.plugins.swagger.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import java.util.concurrent.TimeUnit
+import kotlin.random.Random
+import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.Dispatchers
+import kotlinx.html.*
+import org.slf4j.event.*
+
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+        json()
+    }
+    routing {
+        get("/json/kotlinx-serialization") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
+
---
+++ server/src/main/kotlin/gy/roach/Templating.kt
@@ -0,0 +1,68 @@
+package gy.roach
+
+import com.asyncapi.kotlinasyncapi.context.service.AsyncApiExtension
+import com.asyncapi.kotlinasyncapi.ktor.AsyncApiPlugin
+import com.codahale.metrics.*
+import com.sksamuel.cohort.*
+import com.sksamuel.cohort.cpu.*
+import com.sksamuel.cohort.memory.*
+import dev.hayden.KHealth
+import freemarker.cache.*
+import io.ktor.http.*
+import io.ktor.http.content.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.freemarker.*
+import io.ktor.server.html.*
+import io.ktor.server.http.content.*
+import io.ktor.server.metrics.dropwizard.*
+import io.ktor.server.plugins.cachingheaders.*
+import io.ktor.server.plugins.calllogging.*
+import io.ktor.server.plugins.compression.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.defaultheaders.*
+import io.ktor.server.plugins.openapi.*
+import io.ktor.server.plugins.partialcontent.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.plugins.swagger.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import java.util.concurrent.TimeUnit
+import kotlin.random.Random
+import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.Dispatchers
+import kotlinx.html.*
+import org.slf4j.event.*
+
+fun Application.configureTemplating() {
+    install(FreeMarker) {
+        templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
+    }
+    routing {
+        get("/html-freemarker") {
+            call.respond(FreeMarkerContent("index.ftl", mapOf("data" to IndexData(listOf(1, 2, 3))), ""))
+        }
+        val random = Random(System.currentTimeMillis())
+
+        get("/") {
+            call.respondHtml {
+                leaderboardPage(random)
+            }
+        }
+
+        get("/more-rows") {
+            call.respondHtml {
+                body {
+                    table {
+                        tbody {
+                            randomRows(random)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+data class IndexData(val items: List<Int>)
+
